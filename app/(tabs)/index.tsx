67762d410eda73b86f4d49d92d196ae4f7979dda
Muhammad Aflah <muhammadaflah786@gmail.com>
2024-07-08 20:42:53 +0530

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,103 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import className from "twrnc";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+type Task = {
+  key: string;
+  value: string;
+};
 
-export default function HomeScreen() {
+const index = () => {
+  const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<any>([]);
+  const [editKey, setEditKey] = useState(null);
+
+  const addTask = () => {
+    if (task.trim()) {
+      if (editKey !== null) {
+        setTasks(
+          tasks.map((item: Task) =>
+            item.key === editKey ? { key: item.key, value: task } : item
+          )
+        );
+        setEditKey(null);
+      } else {
+        setTasks([...tasks, { key: tasks.length.toString(), value: task }]);
+      }
+      setTask("");
+    }
+  };
+
+  const deleteTask = (key: any) => {
+    setTasks(tasks.filter((item: Task) => item.key !== key));
+  };
+
+  const editTask = (key: any, value: any) => {
+    setTask(value);
+    setEditKey(key);
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={className`p-4 gap-2 mt-6`}>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <Text style={className`text-2xl font-bold`}>TODO LIST APP</Text>
+      <View
+        style={className` flex-row justify-between items-center gap-2 bg-white shadow-md p-2 rounded-lg`}
+      >
+        <TextInput
+          placeholder="Enter TODOS"
+          style={className` flex-1 p-2 text-lg bg-blue-200 rounded-1g`}
+          onChangeText={setTask}
+          value={task}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <Pressable onPress={addTask}>
+          <Text
+            style={className` p-2 text-white text-lg bg-blue-500 rounded-1g`}
+          >
+            {editKey !== null ? "Update" : "Add"} todo
+          </Text>
+        </Pressable>
+      </View>
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => (
+          <View
+            style={className` gap-2 my-2 bg-green-100 rounded-lg p-2`}
+            key={item.key}
+          >
+            <Text style={className`p-2 rounded-lg bg-blue-200`}>
+              {item.value}
+            </Text>
+            <View style={className` flex-row justify-end items-center gap-2`}>
+              <Pressable onPress={() => editTask(item.key, item.value)}>
+                <Text
+                  style={className`p-2 text-white text-lg bg-blue-500 rounded-lg  text-center `}
+                >
+                  Edit
+                </Text>
+              </Pressable>
+              <Pressable onPress={() => deleteTask(item.key)}>
+                <Text
+                  style={className`p-2 text-white text-lg bg-red-500 rounded-lg  text-center `}
+                >
+                  Delete
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        )}
+      />
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default index;
